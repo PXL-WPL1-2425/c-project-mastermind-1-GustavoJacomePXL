ce6fbf826d44719e85acfe07e76e6ca69c2d8890
GustavoJacomePXL <Gustavo.BarbosaJacome@student.pxl.be>
2024-11-16 15:03:38 +0100

    MasterMind-02

--- Mastermind_project_WPL1/Mastermind_project_WPL1/MainWindow.xaml.cs
+++ Mastermind_project_WPL1/Mastermind_project_WPL1/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Drawing;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,27 @@ namespace Mastermind_project_WPL1
         public MainWindow()
         {
             InitializeComponent();
+
+            Title = GenerateRandomColorCode();
         }
+
+        // methode om random kleurencode te genereren
+        private string GenerateRandomColorCode()
+        {
+            // Beschikbare kleuren
+            string[] colors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
+            Random random = new Random();
+            string[] randomColors = new string[4];
+
+            // Genereer vier willekeurige kleuren
+            for (int i = 0; i < 4; i++)
+            {
+                randomColors[i] = colors[random.Next(colors.Length)];
+            }
+
+            // Combineer de kleuren met een scheidingsteken
+            return string.Join(" - ", randomColors);
+        }
+
     }
 }
