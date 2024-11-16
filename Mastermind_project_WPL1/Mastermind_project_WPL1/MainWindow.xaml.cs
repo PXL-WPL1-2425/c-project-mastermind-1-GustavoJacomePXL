bb6b73ea51de4606e6df7e1c75ede9639a474a4e
GustavoJacomePXL <Gustavo.BarbosaJacome@student.pxl.be>
2024-11-16 15:22:19 +0100

    MasterMind-04

--- Mastermind_project_WPL1/Mastermind_project_WPL1/MainWindow.xaml.cs
+++ Mastermind_project_WPL1/Mastermind_project_WPL1/MainWindow.xaml.cs
@@ -21,7 +21,7 @@ namespace Mastermind_project_WPL1
         {
             InitializeComponent();
 
-            Title = GenerateRandomColorCode();
+            Title = generateRandomColorCode();
 
             // Vul de ComboBoxen met kleuren
             string[] colors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
@@ -29,24 +29,46 @@ namespace Mastermind_project_WPL1
             ComboBox2.ItemsSource = colors;
             ComboBox3.ItemsSource = colors;
             ComboBox4.ItemsSource = colors;
+
+            // event handlers voor kleurenselectie
+            ComboBox1.SelectionChanged += (s, e) => updateLabel(Label1, ComboBox1.SelectedItem.ToString());
+            ComboBox2.SelectionChanged += (s, e) => updateLabel(Label2, ComboBox2.SelectedItem.ToString());
+            ComboBox3.SelectionChanged += (s, e) => updateLabel(Label3, ComboBox3.SelectedItem.ToString());
+            ComboBox4.SelectionChanged += (s, e) => updateLabel(Label4, ComboBox4.SelectedItem.ToString());
         }
 
         // methode om random kleurencode te genereren
-        private string GenerateRandomColorCode()
+        private string generateRandomColorCode()
         {
-            // Beschikbare kleuren
+            // beschikbare kleuren
             string[] colors = { "Rood", "Geel", "Oranje", "Wit", "Groen", "Blauw" };
             Random random = new Random();
             string[] randomColors = new string[4];
 
-            // Genereer vier willekeurige kleuren
+            // genereer vier willekeurige kleuren
             for (int i = 0; i < 4; i++)
             {
                 randomColors[i] = colors[random.Next(colors.Length)];
             }
 
-            // Combineer de kleuren met een scheidingsteken
+            // combineer de kleuren met een scheidingsteken
             return string.Join(" - ", randomColors);
+        }
+
+        // methode om de achtergrondkleur te tonen
+        private void updateLabel(Label label, string color)
+        {
+            label.Content = color;
+            label.Background = color switch
+            {
+                "Rood" => Brushes.Red,
+                "Geel" => Brushes.Yellow,
+                "Oranje" => Brushes.Orange,
+                "Wit" => Brushes.White,
+                "Groen" => Brushes.Green,
+                "Blauw" => Brushes.Blue,
+                _ => Brushes.Transparent
+            };
         }
 
     }
